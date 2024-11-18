d71220b90f0c3658114709475c07bb1c860af792
Илья <iluxapix@gmail.com>
2024-11-18 19:54:45 +0300

    Add files via upload

---
+++ Харин_Часть2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Проходим по строкам от 1 до 9
+        for (int i = 1; i <= 9; i++)
+        {
+            // Проходим по столбцам от 1 до 9
+            for (int j = 1; j <= 9; j++)
+            {
+                // Выводим текущую операцию умножения
+                Console.Write($"{i} x {j} = {i * j}\t");
+            }
+            // Переход на новую строку после завершения строки таблицы
+            Console.WriteLine();
+        }
+    }
+}
+
